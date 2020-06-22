45541b836c4bda1ea9cbffd94fd33dd440ec6da0
Carsten Heins <heinsc@t-online.de>
2020-06-22 10:51:38 +0200

    initialer Stand von Spring initializr

---
+++ src/main/java/de/heins/vokabeltraineronline/VokabeltrainerOnlineApplication.java
@@ -0,0 +1,13 @@
+package de.heins.vokabeltraineronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VokabeltrainerOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VokabeltrainerOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/heins/vokabeltraineronline/VokabeltrainerOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.heins.vokabeltraineronline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VokabeltrainerOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
